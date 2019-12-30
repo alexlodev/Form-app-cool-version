a7cd3e3cd6b837c617f48ae8405462a57ccb68a5
Alexis Lo <alexislo15@hotmail.es>
2019-12-29 23:03:35 -0600

    add folder build

---
+++ build/precache-manifest.495db8e36ba17b101233e4586e6dcce2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa78dfb44f45a678c8c6f1c5be816ca4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4fabc0bd16aaa6e69067",
+    "url": "/static/css/main.b2d131a4.chunk.css"
+  },
+  {
+    "revision": "be2a5494d9e850036151",
+    "url": "/static/js/2.bee70494.chunk.js"
+  },
+  {
+    "revision": "c87d96dbcdc8dd1df6c21457f4947682",
+    "url": "/static/js/2.bee70494.chunk.js.LICENSE"
+  },
+  {
+    "revision": "4fabc0bd16aaa6e69067",
+    "url": "/static/js/main.34818071.chunk.js"
+  },
+  {
+    "revision": "519932569dc402d4594e",
+    "url": "/static/js/runtime-main.78487f88.js"
+  },
+  {
+    "revision": "dbb1f262ee5f283fe8cfe943fd40b684",
+    "url": "/static/media/logo.dbb1f262.svg"
+  }
+]);
